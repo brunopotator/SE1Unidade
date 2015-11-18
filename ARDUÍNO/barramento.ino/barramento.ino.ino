b3d5b11fddb44d2cd7b54fe5dbbfd0cd034072e2
Deyvison Bruno Morais de Abreu <bruno.potator@gmail.com>
2015-11-18 17:30:50 -0300

    Inclusão dos arquivos do Arduíno

---
+++ ARDUÍNO/barramento.ino/barramento.ino.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+#include "HMC5883L.h"
+
+HMC5883L bussola = HMC5883L();
+int erro = 0;
+
+struct Eixos { //deverá ter todos os eixos do sensores
+  int bussX, bussY, bussZ; //eixos da bussola
+};
+
+Eixos eixos;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  erro = bussola.SetScale(1.3); //seta a escala da bussola com o tempo geológico
+  if (erro != 0)
+    bussola.GetErrorText(erro);
+    
+  erro = bussola.SetMeasurementMode(Measurement_Continuous); //modo de medição contínuo
+  if (erro != 0)
+    bussola.GetErrorText(erro);
+}
+
+void loop() {
+  
+  MagnetometerScaled escalado = bussola.ReadScaledAxis(); //usa os valores conforme a escala estabelecida (1.3) e o não escalado
+
+  float heading = atan2(escalado.YAxis, escalado.XAxis); //obtem o quadrante através dos eixos x e y
+
+  float declinationAngle = 0.4072;
+  heading += declinationAngle;
+
+  // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*M_PI;
+    
+  // Check for wrap due to addition of declination.
+  if(heading > 2*M_PI)
+    heading -= 2*M_PI;
+
+  // Convert radianos para graus
+  float headingDegrees = heading * 180/M_PI;
+  
+  eixos.bussX = escalado.XAxis;
+  eixos.bussY = escalado.ZAxis;
+  eixos.bussZ = escalado.ZAxis;
+  
+  enviarEixos();
+
+  delay(104);
+}
+
+void enviarEixos() {
+  int tam = sizeof(eixos); //retorna tamanho em bytes do argumento
+  char buff[tam]; //cria um buffer com o tamanho da estrutura
+  memcpy(&buff, &eixos, tam); //copia a estrutura (destino, origem, tamanho)
+
+  Serial.write('I');
+  Serial.write((uint8_t*)&buff, tam);
+  Serial.write('F');  
+}
+
+
