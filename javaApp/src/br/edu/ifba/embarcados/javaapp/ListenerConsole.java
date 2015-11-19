f098e80b661e8d2c51f2dabacd0112ba5a78d2ec
Deyvison Bruno Morais de Abreu <bruno.potator@gmail.com>
2015-11-19 18:06:01 -0300

    Alteração das operações aritméticas nas condições
    Necessário incluir igualdades nas operações aritméticas para que seja
    possível capturar os ângulos fechados

--- javaApp/src/br/edu/ifba/embarcados/javaapp/ListenerConsole.java
+++ javaApp/src/br/edu/ifba/embarcados/javaapp/ListenerConsole.java
@@ -24,37 +24,37 @@ public class ListenerConsole implements IListenerBussola {
 	}
 	
 	public void imprimirPosicoes(int graus) {
-		if (graus < 45 && graus > 0)
+		if (graus <= 45 && graus > 0)
 		  {
 		    System.out.println("Nordeste");
 		  }
 
-		  else if (graus < 90 && graus > 45)
+		  else if (graus <= 90 && graus > 45)
 		  {
 			  System.out.println("Leste");
 		  }
 		  
-		  else if (graus < 135 && graus > 90)
+		  else if (graus <= 135 && graus > 90)
 		  {
 			  System.out.println("Sudeste");
 		  }
-		  else if (graus < 180 && graus > 135)
+		  else if (graus <= 180 && graus > 135)
 		  {
 			  System.out.println("Sul");
 		  }
-		  else if (graus < 225 && graus > 180)
+		  else if (graus <= 225 && graus > 180)
 		  {
 			  System.out.println("Sudoeste");
 		  }
-		  else if (graus < 270 && graus > 225)
+		  else if (graus <= 270 && graus > 225)
 		  {
 			  System.out.println("Oeste");
 		  }
-		  else if (graus < 315 && graus > 270)
+		  else if (graus <= 315 && graus > 270)
 		  {
 			  System.out.println("Noroeste");
 		  }
-		  else if (graus < 360 && graus > 315)
+		  else if ((graus <= 360 && graus > 315) || graus == 0)
 		  {
 			  System.out.println("Norte");
 		  }
